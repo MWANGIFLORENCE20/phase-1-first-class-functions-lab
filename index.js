03a137c9fa284c30951c37f2fcbd5a149e2b2c11
florence wangechi <florencewangechim@gmail.com>
2023-03-16 23:20:29 +0300

    Complete Assingment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const drivers =["Antonia", "Nuru", "Amari" , "Mo"];
+const returnFirstTwoDrivers = function(){
+    return ["Antonia", "Nuru"];
+}
+
+ const returnLastTwoDrivers = function(){
+    return ["Amari" , "Mo"]
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+
+ function createFareMultiplier(multiplier){
+    return function(value){
+    return value * multiplier
+ };
+ }
+
+ const fareDoubler = createFareMultiplier(2);
+
+ const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers , selectingdrivers){
+    return selectingdrivers(drivers);
+}
+
+
